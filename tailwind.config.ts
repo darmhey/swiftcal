645aaaff01a57d12ed66247a0052a6d429d9af2b
Damilola <johnasco19@gmail.com>
2025-02-06 13:16:38 +0100

    hero section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        main: "#ECE6E8",
+        accent: "#FF5A00",
       },
     },
   },
