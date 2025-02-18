cba90aeae093b2154ecc0961241f5c707e2666fe
Damilola <johnasco19@gmail.com>
2025-02-18 13:01:40 +0100

    routes

---
+++ backend/src/routes/appointmentRoutes.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import {
+  getAppointments,
+  createAppointment,
+} from "../controllers/appointmentController";
+
+const router = express.Router();
+
+router.get("/", getAppointments);
+router.post("/", createAppointment);
+
+export default router;
