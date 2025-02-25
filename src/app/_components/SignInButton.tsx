34e3ce78ee1a89fcf61c47647cc8b8a68361950a
Damilola <johnasco19@gmail.com>
2025-02-25 11:56:45 +0100

    passport setup till callback1

--- src/app/_components/SignInButton.tsx
+++ src/app/_components/SignInButton.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 function SignInButton() {
   return (
     <form>
-      <Link href="/schedule">
+      <Link href="/http://127.0.0.1:3002/api/v1/auth/google">
         <button className="flex items-center gap-6 text-lg border border-primary-300 px-10 py-4 font-medium">
           <Image
             src="google.svg"
